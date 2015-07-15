31a39e84f6225755f87a8175e1c3b8ec23bdaff5
Yongjie <yongjiex.gu@intel.com>
2015-07-16 07:37:04 +0800

    fix bug that cmdline set_up_all failed because of time out
    Signed-off-by: Yongjie <yongjiex.gu@intel.com>

--- tests/TestSuite_cmdline.py
+++ tests/TestSuite_cmdline.py
@@ -58,7 +58,7 @@ class TestCmdline(TestCase):
         # Run cmdline app
         cores = self.dut.get_core_list('1S/1C/1T')
         coreMask = dts.create_mask(cores)
-        self.dut.send_expect("./examples/cmdline/build/app/cmdline -n 1 -c " + coreMask, "> ", 5)
+        self.dut.send_expect("./examples/cmdline/build/app/cmdline -n 1 -c " + coreMask, "> ", 10)
 
     def set_up(self):
         """
